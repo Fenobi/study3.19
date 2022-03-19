af368da23c65c6a21c4922ccafffa72a91e94852
付青云 <1366773912@qq.com>
2022-03-19 23:55:42 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//#include <assert.h>
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(src != NULL||dest != NULL);
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[20] = "***************";
+//	char arr2[] = "Hello";
+//	my_strcpy(arr1,arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int num = 10;
+//	int* const p = &num;
+//	*p = 20;
+//	p = 20;
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//#include <string.h>
+//#include <assert.h>
+//size_t my_strlen( const char* stc)//size_t=unsigned int
+//{
+//	assert(stc);
+//	int i = 0;
+//	while (*stc++)
+//	{
+//		i++;
+//	}
+//	return i;
+//}
+//int main()
+//{
+//	char arr[] = "123456789";
+//	//int a = strlen(arr);
+//	int a = my_strlen(arr);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
